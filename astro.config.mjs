ffdffe54335e33e8ec09fb15f893261c8705141f
John Dunning <fw@johndunning.com>
2024-04-17 16:13:32 -0700

    Make blog links root relative

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,14 @@ import icon from "astro-icon";
 
 export default defineConfig({
 	//	site: "https://www.sfcivictech.org/"
-	//	base: "/",
+	//	base: "",
 	site: "https://sfbrigade.github.io",
 	base: "/astro-static-site/",
+	trailingSlash: "ignore",
 	compressHTML: false,
 	integrations: [
 		icon({
 			include: {
-				// Include all `mdi` icons in the bundle
-				// mdi: ["*"],
 				// Include only specific `fa` icons in the bundle
 				fa: ["twitter", "facebook", "linkedin", "github", "slack", "meetup"],
 			},
