a7f8ee9be84165ac36ff6a62091e16137eb5e21c
John Dunning <fw@johndunning.com>
2024-05-04 16:42:12 -0700

    Convert image in frontmatter to full path in content/config.ts
    Simplify getBlogImage().
    Update description Page.astro.

--- src/content/config.ts
+++ src/content/config.ts
@@ -21,7 +21,15 @@ const blogCollection = defineCollection({
 				return date.toDate();
 			})
 			.optional(),
-		image: z.string().optional(),
+		image: z.string()
+			.transform((value) => {
+				if (!value) {
+					return z.NEVER;
+				}
+
+				return "/src/assets/blog/" + value.replace(/^\/img\/uploads\//, "");
+			})
+			.optional(),
 		image_alt: z.string().optional(),
 		image_list_only: z.boolean().optional(),
 		description: z.string().optional(),
