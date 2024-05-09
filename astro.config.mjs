ab6693beb89ceec46a519e98aa120776c7010e79
Code4Dopamine <11954656+Code4Dopamine@users.noreply.github.com>
2024-05-08 19:13:35 -0700

    Added Tailwind and tested box shadow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-	//	site: "https://www.sfcivictech.org/"
-	//	base: "",
-	site: "https://sfbrigade.github.io",
-	base: "/astro-static-site/",
-	trailingSlash: "ignore",
-	compressHTML: false,
-	integrations: [
-		icon({
-			include: {
-				// Include only specific `fa` icons in the bundle
-				fa: ["twitter", "facebook", "linkedin", "github", "slack", "meetup"],
-			},
-		}),
-	],
+  //	site: "https://www.sfcivictech.org/"
+  //	base: "",
+  site: "https://sfbrigade.github.io",
+  base: "/astro-static-site/",
+  trailingSlash: "ignore",
+  compressHTML: false,
+  integrations: [icon({
+    include: {
+      // Include only specific `fa` icons in the bundle
+      fa: ["twitter", "facebook", "linkedin", "github", "slack", "meetup"]
+    }
+  }), tailwind()]
 });
