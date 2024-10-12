fd173f0e727a5e67c1639a62f30d463729bcd3b8
John Dunning <fw@johndunning.com>
2024-10-11 20:41:55 -0700

    Fix RSS feed dates

--- src/utils/dayjs.ts
+++ src/utils/dayjs.ts
@@ -8,12 +8,12 @@ dayjs.extend(timezone);
 dayjs.extend(customParseFormat);
 
 // interpret all date strings as Pacific time, in case this gets built in some
-// other time zone
+// other time zone.  make sure to call the .tz() method instead of just dayjs().
 dayjs.tz.setDefault("America/Los_Angeles");
 
 export default dayjs;
 
-export const parseYMD = (date: string) => dayjs(date, "YYYY-MM-DD", true);
+export const parseYMD = (date: string) => dayjs.tz(date);
 export const parseYMDToDate = (date: string) => parseYMD(date).toDate();
-export const formatShortMDY = (date: Date) => date ? dayjs(date).format("MMM D, YYYY") : "";
-export const formatRSSDate = (date: Date) => date ? dayjs(date).format("ddd, D MMM YYYY HH:mm:ss ZZ") : "";
+export const formatShortMDY = (date: Date) => date ? dayjs.tz(date).format("MMM D, YYYY") : "";
+export const formatRSSDate = (date: Date) => date ? dayjs.tz(date).format("ddd, D MMM YYYY HH:mm:ss ZZ") : "";
