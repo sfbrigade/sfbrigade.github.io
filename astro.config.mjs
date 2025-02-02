de44597703f2b57125d5ee1412252ae14c615fcf
John Dunning <fw@johndunning.com>
2025-02-01 17:13:12 -0800

    Fix typo in blog post URL
    Add a redirect to the config to map the old URL with the typo to the corrected one, on the off chance anyone has that bookmarked.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,23 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import icon from "astro-icon";
 
+const oldCivcURL = "/blog/2019-06-22-introducing-civc-tech-to-san-franciscos-underserved-communities";
+
 export default defineConfig({
 	site: "https://www.sfcivictech.org",
 	base: "",
 	trailingSlash: "ignore",
 	compressHTML: true,
+	redirects: {
+		// redirect old post URL to a new one without the typo
+		[oldCivcURL]: oldCivcURL.replace("civc", "civic"),
+	},
 	integrations: [
 		react(),
 		icon({
 			iconDir: "src/assets/icons",
 			include: {
-				// Include only specific `fa` icons in the bundle
+				// include only specific `fa` icons in the bundle
 				fa: [
 					"facebook",
 					"linkedin",
