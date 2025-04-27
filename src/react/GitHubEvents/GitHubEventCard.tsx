2bab0ea1b2eb9d4d8758fd62e03e89179eaa10ed
John Dunning <fw@johndunning.com>
2025-04-26 20:11:12 -0700

    Redesign get-started.astro page
    Use large digits for the getting started steps.
    Add BigHeading.astro component to show large uppercase text.
    Move the join the conversation item first.
    Tweak GitHubEventCard.tsx to use stone colors.

--- src/react/GitHubEvents/GitHubEventCard.tsx
+++ src/react/GitHubEvents/GitHubEventCard.tsx
@@ -26,12 +26,14 @@ export default function GitHubEventCard({
 			className="
 				w-[calc((100cqw-2*theme(spacing.10)-1px)/3)]
 				block aspect-square p-4
-				text-center no-underline text-gray-600 dark:text-gray-400
+				text-center no-underline
+				text-stone-600 dark:text-stone-400
 				flex flex-col items-center justify-between flex-shrink-0
-				bg-gray-100 dark:bg-gray-800
-				border border-gray-300 dark:border-gray-700
+				bg-stone-50 dark:bg-stone-800
+				border border-stone-300 dark:border-stone-700
 				transition-colors duration-300 ease-in-out
-				hover:bg-gray-200 dark:hover:bg-gray-700 hover:text-gray-600 dark:hover:text-gray-400
+				hover:bg-stone-200 dark:hover:bg-stone-700
+				hover:text-stone-700 dark:hover:text-stone-400
 				snap-start
 				overflow-hidden break-words
 			"
@@ -42,17 +44,17 @@ export default function GitHubEventCard({
 					alt={`${username}'s avatar`}
 					className="w-14 h-14 aspect-square rounded-full mb-1"
 				/>
-				<h3 className="m-0 mb-1 text-base font-semibold">{username}</h3>
+				<h3 className="m-0 mb-1 text-base text-muted font-semibold">{username}</h3>
 			</header>
 			<p
 				title={messageTooltip}
-				className="text-sm leading-[1.2] m-0 overflow-hidden text-ellipsis break-words line-clamp-2 lg:line-clamp-3"
+				className="text-sm leading-[1.2] font-condensed m-0 overflow-hidden text-ellipsis break-words line-clamp-2 lg:line-clamp-3"
 					// break-words doesn't actually add word-break: break-word, which is needed to handle the long lines
 				style={{ wordBreak: "break-word" }}
 			>
 				{message}
 			</p>
-			<footer className="text-xs  opacity-75">
+			<footer className="text-xs text-muted">
 				<h4 className="mt-1 mb-0 font-bold">{repo}</h4>
 				<time
 					dateTime={timestamp}
